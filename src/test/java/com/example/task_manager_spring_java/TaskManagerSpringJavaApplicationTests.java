5653221bcfa82121753d7bf477d1ce91c57fec69
vedprakash <1995ved@gmail.com>
2023-03-30 23:00:24 +0530

    Initial commit

---
+++ src/test/java/com/example/task_manager_spring_java/TaskManagerSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
